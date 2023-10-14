724c6faed44011d1a17deac0ef018b76674687c2
bhavanikairika3 <147837665+bhavanikairika3@users.noreply.github.com>
2023-10-14 10:12:36 +0530

    code.java

--- code.java
+++ code.java
@@ -1,26 +1,36 @@
-public class PangramChecker {
-    public static boolean isPangram(String str) {
+public class PangramChecker 
+{
+    public static boolean isPangram(String str) 
+    {
         str = str.toLowerCase();
         boolean[] isAlphabetPresent = new boolean[26];
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) 
+        {
             char ch = str.charAt(i);
-            if (Character.isAlphabetic(ch)) {
+            if (Character.isAlphabetic(ch)) 
+            {
                 isAlphabetPresent[ch - 'a'] = true;
             }
         }
-        for (boolean isPresent : isAlphabetPresent) {
-            if (!isPresent) {
+        for (boolean isPresent : isAlphabetPresent) 
+        {
+            if (!isPresent)
+            {
                 return false;
             }
         }
         return true;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         String sentence = "Pack my box with five dozen liquor jugs";
         boolean isPangram = isPangram(sentence);
-        if (isPangram) {
+        if (isPangram) 
+        {
             System.out.println("The sentence is a pangram.");
-        } else {
+        } 
+        else 
+        {
             System.out.println("The sentence is not a pangram.");
         }
     }
